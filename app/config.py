f06226a079f62b745c7da504fbfee7aeffeff02a
Keoni Gandall <koeng101@gmail.com>
2019-04-18 13:14:40 -0700

    updated config with buckets

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-#import boto3
+import boto3
 import os
 SECRET_KEY = os.environ['SECRET_KEY']
 URL = os.environ['URL']
@@ -7,4 +7,11 @@ LOGIN_KEY = os.environ['LOGIN_KEY']
 PREFIX = os.environ['PREFIX'] # BBF10K_
 
 FG_API = os.environ['FG_API']
+session = boto3.session.Session()
+SPACES = session.client('s3',
+                        region_name=os.environ['REGION_NAME'],
+                        endpoint_url=os.environ['ENDPOINT_URL'],
+                        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+                        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
+BUCKET = os.environ['BUCKET']
 
